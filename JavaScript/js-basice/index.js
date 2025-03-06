5f1063e9eac7fe8ef532b312bafba12c0c913f7b
quartz <ppp_melody@163.com>
2025-03-06 15:24:58 +0800

    1 section left to review

--- JavaScript/js-basice/index.js
+++ JavaScript/js-basice/index.js
@@ -1,19 +1,19 @@
-let circle = {
-  radius: 10,
-  draw() {
-    console.log("Drawing a circle");
-  },
-};
+const courses = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+];
 
-let another = {
-  radius: 5,
-  color: "yellow",
-};
+// includes() 无法匹配具有相同内容但不同引用的对象
+console.log(courses.includes({ id: 1, name: "a" })); // 输出: false
 
-// 使用 Object.assign() 合并对象
-let merged1 = Object.assign({}, circle, another);
-console.log(merged1);
+// 使用 find() 根据条件查找对象
+const courseA = courses.find(function (course) {
+  return course.name === "a";
+});
+console.log(courseA); // 输出: { id: 1, name: "a" }
 
-// 使用扩展运算符合并对象
-let merged2 = { ...circle, ...another };
-console.log(merged2);
+// 使用 findIndex() 查找符合条件的对象索引
+const indexB = courses.findIndex(function (course) {
+  return course.name === "b";
+});
+console.log(indexB); // 输出: 1
