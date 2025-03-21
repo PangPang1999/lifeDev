9d3b3f97db1f8998de46336e1afdc0d22670d2be
fangtt <heycarline@gmail.com>
2025-03-21 09:35:32 +0800

    Ts:initial

--- Java/HelloWorld/src/main/java/com/pang/Employee.java
+++ Java/HelloWorld/src/main/java/com/pang/Employee.java
@@ -1,10 +1,30 @@
 package com.pang;
 
 public class Employee {
-    int baseSalary = 30_000;
-    int hourlyRate = 20;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateSalary(int extraHours) {
-        return baseSalary + (hourlyRate * extraHours);
+        return getBaseSalary() + (getHourlyRate() * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Base Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("HourlyRate cannot be 0 or less.");
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
     }
 }
