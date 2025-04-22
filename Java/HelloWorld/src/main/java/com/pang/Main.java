0a5f6042f44669d1e54a16a8453292a14b692dc1
quartz <ppp_melody@163.com>
2025-04-22 15:14:37 +0800

    Java: process and thread

--- Java/HelloWorld/src/main/java/com/pang/Main.java
+++ Java/HelloWorld/src/main/java/com/pang/Main.java
@@ -1,5 +1,7 @@
 package com.pang;
 
+import com.pang.concurrency.DownloadFileTask;
+import com.pang.concurrency.MyThread;
 import com.pang.generics.*;
 import com.pang.lambdas.LambdasDemo;
 import com.pang.streams.StreamsDemo;
@@ -10,6 +12,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        StreamsDemo.show();
+        System.out.println(Thread.currentThread().getName());
+        // for (int i = 0; i < 10; i++) {
+        //     Thread thread = new Thread(() -> System.out.println("Download a file" + Thread.currentThread().getName()));
+        //     thread.start();
+        // }
+        for (int i = 0; i < 10; i++) {
+            MyThread myThread = new MyThread();
+            myThread.start();
+        }
     }
 }
---
+++ Java/HelloWorld/src/main/java/com/pang/concurrency/MyThread.java
@@ -0,0 +1,8 @@
+package com.pang.concurrency;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("MyThread.run"+Thread.currentThread().getName());
+    }
+}
