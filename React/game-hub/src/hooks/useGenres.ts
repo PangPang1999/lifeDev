ed7ad9599148025020f879f67fe41e82d3916480
fangtt <heycarline@gmail.com>
2025-05-23 22:23:29 +0800

    React:shipping static data

--- React/game-hub/src/hooks/useGenres.ts
+++ React/game-hub/src/hooks/useGenres.ts
@@ -1,9 +1,15 @@
-import useData from "./useData";
-
+import staticGenres from "../data/genres";
 export interface Genres {
   id: number;
   name: string;
   image_background: string;
 }
-const useGenres = () => useData<Genres>("/genres");
+// const useGenres = () => useData<Genres>("/genres");
+const useGenres = () => {
+  return {
+    data: staticGenres as Genres[],
+    isLoading: false,
+    error: null,
+  };
+};
 export default useGenres;
